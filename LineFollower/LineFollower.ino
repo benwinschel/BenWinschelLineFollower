890c03a245d9cdedab52df2db2e4939ab70a92e4
Ben Winschel <ben@winschel.com>
2017-09-20 13:17:51 -0700

    Refactored maze code

--- LineFollower/LineFollower.ino
+++ LineFollower/LineFollower.ino
@@ -12,6 +12,62 @@ int lturns = 0;
 int turnaround = 0;
 int regSpeed = 125;
 
+
+void straight(){
+  turnaround = 0;
+  m1.run(-regSpeed);
+  m2.run(regSpeed);
+}
+
+void adjleft(){
+  turnaround = 0;
+  m1.run(-regSpeed);
+  m2.run(regSpeed + 50);
+}
+
+
+void adjright(){
+  turnaround = 0;
+  m2.run(regSpeed);
+  m1.run(-regSpeed - 50);    
+}
+void deadend(){
+  turnaround++;
+  if(turnaround>20){
+    do{
+      m1.run(2*regSpeed);
+      m2.run(2*regSpeed);
+    }while(left.readSensors()!=2 && right.readSensors()!=1);
+  }
+}
+void leftturn(){
+  turnaround = 0;
+  lturns++;
+  rturns = 0;
+  delay(500);
+  m1.stop();
+  do{
+  m2.run(regSpeed);
+  }while(left.readSensors() != 2 && right.readSensors() != 1);
+}
+void rightturn(){
+  turnaround = 0;
+  rturns++;
+  lturns = 0;
+  delay(500);
+  m2.stop();
+  do{
+    m1.run(-regSpeed);
+  }while(left.readSensors() != 2 && right.readSensors() != 1);
+}
+
+void backup(){
+  m1.run(regSpeed);
+  m2.run(-regSpeed);
+}
+
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -22,91 +78,44 @@ void loop() {
   // put your main code here, to run repeatedly:
   
   if(left.readSensors()==2 && right.readSensors()==1){
-      turnaround = 0;
-      m1.run(-regSpeed);
-      m2.run(regSpeed);
+      straight();
   }
   if(right.readSensors()==2 && rturns<=3){
-      turnaround = 0;
-      rturns++;
-      lturns = 0;
-      delay(500);
-      m2.stop();
-      do{
-        m1.run(-regSpeed);
-     }while(left.readSensors() != 2 && right.readSensors() != 1);
+      rightturn();
   }
   if(left.readSensors()==0 && lturns<=3){
-      turnaround = 0;
-      lturns++;
-      rturns = 0;
-      delay(500);
-      m1.stop();
-      do{
-        m2.run(regSpeed);
-    }while(left.readSensors() != 2 && right.readSensors() != 1);
+      leftturn();
   }
   if(left.readSensors()==0 && right.readSensors()==0){
-    turnaround = 0;
-    m1.run(-regSpeed);
-    m2.run(regSpeed);
+    straight();
     delay(1000);
     if(left.readSensors()==0 && right.readSensors()==0){
        exit(0); 
     }else{
       if(left.readSensors()==2 || right.readSensors()==1){
-        m1.run(-regSpeed);
-        m2.run(regSpeed);
+        straight();
       }else{
-      
-      m1.run(regSpeed);
-      m2.run(-regSpeed);
-      delay(2000);
-      
-      if(lturns>=3){
-        lturns = 0;
-        rturns = 1;
-        do{
-          m2.stop();
-          m1.run(-regSpeed);
-        }while(left.readSensors() != 2 && right.readSensors() != 1);
-      }
-      if(rturns>=3){
-        rturns = 0;
-        lturns = 1;
-        do{
-          m1.stop();
-          m2.run(regSpeed);
-        }while(left.readSensors() != 2 && right.readSensors() != 1);
-      }
-      if(rturns<3 && lturns<3){
-        rturns++;
-        do{
-          m2.stop();
-          m1.run(-regSpeed);
-        }while(left.readSensors() != 2 && right.readSensors() != 1);
+        backup();
+        delay(2000);
+        if(lturns>=3){
+          rightturn();
+        }
+        if(rturns>=3){
+          leftturn();
+        }
+        if(rturns<3 && lturns<3){
+          rightturn();
+        }
       }
     }
   }
-  }
   if(left.readSensors()==3 && right.readSensors()==3){
-     turnaround++;
-     Serial.println("DANGER WILL ROBINSON");
-     if(turnaround>1){
-      do{
-        m1.run(2*regSpeed);
-        m2.run(2*regSpeed);
-      }while(left.readSensors()!=2 && right.readSensors()!=1);
-     }
+     deadend();
   }
   if(left.readSensors()==3 && right.readSensors()!=3){
-    turnaround = 0;
-    m2.run(regSpeed);
-    m1.run(-regSpeed - 50);
+    adjright();
   }
   if(right.readSensors()==3 && left.readSensors()!=3){
-    turnaround = 0;
-    m1.run(-regSpeed);
-    m2.run(regSpeed + 50);
+    adjleft();
   }
 }
